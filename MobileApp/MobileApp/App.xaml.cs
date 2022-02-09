674ad1012bac73ebdfe90b694219be7b1bc25701
lmorar00 <lmorar00@hotmail.com>
2022-02-08 21:52:59 -0600

    Finish lab-5

--- MobileApp/MobileApp/App.xaml.cs
+++ MobileApp/MobileApp/App.xaml.cs
@@ -1,26 +1,14 @@
 ﻿using MobileApp.Views;
-using Xamarin.Forms;
 
 namespace MobileApp
 {
-	public partial class App
-	{
-		public App()
-		{
-			InitializeComponent();
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new ContentPage { Content = new Label { Text = "Hello, World!", VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center } };
-		}
-
-		private Page UseTabs()
-		{
-			TabsPage tabs;
-			MainPage = tabs = new TabsPage();
-
-			tabs.Children.Add(new FontsView() { Title = "Font Families" });
-			tabs.Children.Add(new FontSample("Lobster") { Title = "Font sample" });
-
-			return tabs;
-		}
-	}
+            MainPage = new MyLocationPage();
+        }
+    }
 }
